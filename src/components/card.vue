5504a607cb4bd8f26767cbf534f03f06c99fe209
tristan brochard <brochardtristan@gmail.com>
2021-01-09 11:41:45 +0100

    project done

--- src/components/card.vue
+++ src/components/card.vue
@@ -2,7 +2,11 @@
   <div class="card group">
     <img class="card-img" :src="require(`@/assets/img/${imgLink}`)" alt="">
     <h2 class="card-title">{{ title }}</h2>
-    <router-link class="card-button" :to="projectLink">Voir le projet</router-link>
+    <div class="w-80 mx-auto">
+      <p class="text-primary mb-6 text-center group-hover:text-secondary">{{ technologies }}</p>
+    </div>
+    <a class="card-button" :href="projectLink" v-show="showProjectButton">le site</a>
+    <a class="card-button" :href="githubLink">github</a>
   </div>
 </template>
 
@@ -13,7 +17,15 @@ export default {
     "title",
     "projectLink",
     "imgLink",
+    "technologies",
+    "githubLink",
+    "showProjectButton",
   ],
+  data () {
+    return {
+      displayButton: true,
+    }
+  }
 }
 </script>
 
@@ -21,7 +33,7 @@ export default {
 <style>
 @layer utilities {
   .card {
-    @apply mr-5 mt-5 border-2 border-white hover:bg-lightGrey rounded-2xl flex flex-col;
+    @apply mr-5 mt-3 pb-2 border-2 border-white hover:bg-lightGrey rounded-2xl flex flex-col;
   }
   .card-img {
     @apply rounded-xl w-80 h-40;
@@ -30,7 +42,7 @@ export default {
     @apply text-white text-center mt-6 group-hover:text-black;
   }
   .card-button {
-    @apply text-white text-center mt-10 mb-10 border-2 border-white rounded-xl mx-6 py-1 group-hover:text-black group-hover:border-black hover:bg-primary;
+    @apply text-white text-center mb-2 border-2 border-white rounded-xl mx-6 py-1 group-hover:text-black group-hover:border-black hover:bg-primary;
   }
 }
 </style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,14 +4,14 @@
 
     <div class="container flex flex-row flex-wrap justify-center ml-2 md:ml-10 primary-font">
 
-      <Card class="apparition element1" title="passion mouche" imgLink="passion.jpg" projectLink="/passion-mouche" />
-      <Card class="apparition element2" title="qovery" imgLink="qovery-hero-header.jpg" projectLink="/qovery" />
-      <Card class="apparition element3" title="groupomania" imgLink="groupomania-header.jpg" projectLink="/groupomania" />
-      <Card class="apparition element4" title="sopekocko" imgLink="sopekocko.jpg" projectLink="/sopekocko" />
-      <Card class="apparition element5" title="orinoco" imgLink="Orinoco-header.jpg" projectLink="/orinoco" />
-      <Card class="apparition element6" title="la chouette agence" imgLink="Chouette-header.jpg" projectLink="/chouette-agence" />
-      <Card class="apparition element7" title="oh my food" imgLink="food-head.jpg" projectLink="/ohmyfood" />
-      <Card class="apparition element8" title="CV" imgLink="cv-head.jpg" projectLink="/cv" />
+      <Card class="apparition element1" title="Portfolio" imgLink="portfolio.jpg" technologies="Vue.JS" githubLink="#" />
+      <Card class="apparition element2" title="passion mouche" imgLink="passion.jpg" technologies="Vue.JS" showProjectButton="displayButton" projectLink="https://dev-passion-mouche.netlify.app/#/" githubLink="https://github.com/fullak/passion-mouche" />
+      <Card class="apparition element3" title="qovery" imgLink="qovery-hero-header.jpg" technologies="Vue.JS" githubLink="https://github.com/fullak/qovery" />
+      <Card class="apparition element4" title="groupomania" imgLink="groupomania-header.jpg" technologies="Vue.JS | NodeJS | MySQL" githubLink="https://github.com/fullak/groupomania" />
+      <Card class="apparition element5" title="sopekocko" imgLink="sopekocko.jpg" technologies="NodeJS | MongoDB" githubLink="https://github.com/fullak/sopekocko" />
+      <Card class="apparition element6" title="orinoco" imgLink="Orinoco-header.jpg" technologies="HTML5 | CSS3 | JS" githubLink="https://github.com/fullak/Orinoco" />
+      <Card class="apparition element7" title="la chouette agence" imgLink="Chouette-header.jpg" technologies="SEO" githubLink="https://github.com/fullak/la_chouette_agence" />
+      <Card class="apparition element8" title="CV" imgLink="cv-head.jpg" technologies="HTML5 | CSS3" githubLink="https://github.com/fullak/fullak.github.io" />
 
     </div>
   </div>
